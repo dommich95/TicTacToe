b46b57b9d7d9586ae45ebd995eb69b03cca0299e
dommich95 <dominikmcihel1995gmail.com>
2022-07-04 22:02:13 +1000

    pro0

--- js/pro0.js
+++ js/pro0.js
@@ -1,34 +1,78 @@
 $(document).ready(function(){
-    
-    
-    // Your app must:
-    // * **Switch turns** between X and O (or whichever markers you select)
-    // * **Visually display which side won** if a player gets three in a row or show a draw/"cat’s game" if neither wins
-    // * **Include separate HTML / CSS / JavaScript files**
-    // * Stick with **KISS (Keep It Simple Stupid)** and **DRY (Don't Repeat Yourself)** principles
-    // * Use **Javascript** for **DOM manipulation**
-    // * **Deploy your game online**, where the rest of the world can access it
-    // * Use **semantic markup** for HTML and CSS (adhere to best practices)
-    
 
 const possibleWins=[
-    ["A1", "A2", "A3"], ["B1", "B2", "B3"], ["C1", "C2", "C3"],
-    ["A1", "B1"]
-]
+    ['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'],
+    ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'],
+    ['1', '5', '9'], ['7', '5', '3']
+];
 
-  $('.feeld').on('click', function(){
-      function turns(){
-          switch(player){
-            case 1: 
-              $('.feeld').html('.playerOne').html("X");
-              break;
-            case 2:
-                $('.feeld').html('.playerTwo').html("O");
-          }
-      }
-       turns($('.playerOne'), $('.playerTwo'))
-  })
+let playerOne=true;
 
+$('.feeld').on('click', function(e){
+       
+    if ($(this).html()=="X" || $(this).html() =="O"){
+        return;
+    }else if(playerOne== true){
+        $(this).html("X").addClass("X")
+        var idArrayX = [];
+        $('.X').each(function () {
+        idArrayX.push(this.id);
+        parseInt(idArrayX)
+        var id1=idArrayX.join('')
+        console.log(id1)
+        for(let i=0;i<possibleWins.length;i++){
+            let index= possibleWins[i];
+            let posWin=index.join('');
+            if(id1==posWin){
+                $('.win1').html(+1)
+        }}
+    })
+        }else if(playerOne== false){
+            $(this).html("O").addClass("O")
+            var idArrayO = [];
+            $('.O').each(function () {
+            idArrayO.push(this.id);
+            parseInt(idArrayO)
+            var id2=idArrayO.join('')
+            console.log(id2)
+
+       for(let i=0;i<possibleWins.length;i++){
+        let index= possibleWins[i];
+        let posWin=index.join('');
+        // console.log(posWin)
+        if(id2==posWin){
+            $('.win2').html(+1)
+        }} 
+     });
+    };
+     
+     playerOne= !playerOne;
+     
+     
+})
+
+//   const winner= function(){
+//     index=[];
+//     var idArrayX = [];
+//     $('.X').each(function () {
+//         idArrayX.push(this.id);
+//         console.log(idArrayX)
+//     });
+//      var idArrayO = [];
+//     $('.O').each(function () {
+//         idArrayO.push(this.id);
+//         console.log(idArrayO)
+//     });
+
+//     for(let i=0;i<possibleWins.length;i++){
+//         let index= possibleWins[i];
+//         if(index==idArrayX){
+//             alert ('X wins')
+//         }else if( index===idArrayO){
+//             alert('O wins')
+//     }
+// }}
+// winner()
 
 
 
